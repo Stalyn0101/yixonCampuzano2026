539b291c5a5f801983c2a8b32d58f3d7724a4c79
Stalyn0101 <yixon11@gmail.com>
2024-11-07 13:24:31 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/yixoncampuzano2026/YixonCampuzano2026.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.yixoncampuzano2026;
+
+/**
+ *
+ * @author USER
+ */
+public class YixonCampuzano2026 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
